692bdc6341b9e9dd70d4eea5d3d70a6f892e8ac6
Dave Ansell <dave@sciansell.co.uk>
2019-02-05 18:56:34 +0000

    move lights into its own module

--- node_helper.js
+++ node_helper.js
@@ -8,13 +8,13 @@ module.exports = NodeHelper.create({
     let DEBUG = payload["debug"];
     let blankTime = payload["blankTime"];
     var thethis = this;
-    var lights = require("child_process").spawn("sudo", [
-                       "python3",
-                        __dirname + "/lights.py",
+  //  var lights = require("child_process").spawn("sudo", [
+   //                    "python3",
+     //                   __dirname + "/lights.py",
 
-    ]);
-    lights.stdin.setEncoding('utf-8');
-    lights.stdin.write("0\n");
+   // ]);
+   // lights.stdin.setEncoding('utf-8');
+   // lights.stdin.write("0\n");
 
     // creating demon spawn
     var child = require("child_process").spawn(__dirname + "/facetracker_haar.py"
@@ -23,20 +23,21 @@ module.exports = NodeHelper.create({
     // using arrow function to keep "this" reference of outer scope
     child.stdout.on("data", (data) => {
 	  data=data.toString();
-	  console.log("data=",data);
+	  if(DEBUG)
+	    console.log("data=",data);
 	  lastDetect = Date.now();
 	  this.sendSocketNotification("PAGE_ACTIVATE", 1);
-	  lights.stdin.write("1\n");
+//	  lights.stdin.write("1\n");
     });
-    setInterval(function(){
-      	if(Date.now()-lastDetect > blankTime){
-	  thethis.sendSocketNotification("PAGE_DEFAULT", 0);
-	  lights.stdin.write("0\n");
-      }
-    }, 500);
+ //   setInterval(function(){
+ //     	if(Date.now()-lastDetect > blankTime){
+//	  thethis.sendSocketNotification("PAGE_DEFAULT", 0);
+//	  lights.stdin.write("0\n");
+ //     }
+ //   }, 500);
 
     child.stdout.on("exit", function(exitCode) {
-      lights.stdin.write("q\n");
+  //    lights.stdin.write("q\n");
       console.log(exitCode);
     });
   },
